ed6d95ad5266b8f82b56b2a3b9831451b8aec737
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-02-06 15:18:19 -0500

    added todos route to be handled by express

--- web/routes/routes.js
+++ web/routes/routes.js
@@ -1,9 +1,20 @@
-const testRoute = require('./testRoute');
+const { celebrate } = require('celebrate');
+
+const testRoute = require('./test');
+
+const todoRoute = require('./todo');
+const todoValidator = require('./validators/todo');
 
 module.exports = function (router) {
 
   // test route to make sure everything is working http://localhost:8080/api
-  router.get('/test', testRoute.testRoute);
-  router.get('/testCreate', testRoute.testCreateRoute);
+  router.get('/test'      ,         testRoute.testRoute);
+  router.get('/testCreate',   testRoute.testCreateRoute);
 
+  // Todo Routes
+  router.get(     '/todo'        ,                                   todoRoute.getAll);         // all todos
+  router.get(     '/todo/:id'    , celebrate(todoValidator.getOne),  todoRoute.getOne);         // single todo
+  router.post(    '/todo'        , celebrate(todoValidator.create),  todoRoute.create);         // create todo
+  router.put(     '/todo/:id'    , celebrate(todoValidator.update),  todoRoute.update);         // update a todo
+  router.delete(  '/todo/:id'    , celebrate(todoValidator.remove),  todoRoute.remove);         // delete todo
 };
