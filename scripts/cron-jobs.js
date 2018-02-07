c1a08ca13c1885cd41cf1c54f738001f7dbb58ce
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-02-06 21:08:33 -0500

    updated emailjob for cron

--- scripts/cron-jobs.js
+++ scripts/cron-jobs.js
@@ -1,7 +1,11 @@
 const cron = require('node-cron');
-
+const moment = require('moment');
+const helper = require('./helper.js')
+// runs every one min for now
 const emailJob = cron.schedule('*/1 * * * *', () => {
-  console.log('running a task every minute');
+  console.log('Beginning to send our reminders (if any) at', moment().format('MMM DD YYYY hh:mm:ss a'));
+
+  helper.sendReminders();
 }, false);
 
 module.exports = {
